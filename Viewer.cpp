ed55bfef4c8d3e7d297c40e7b4e2bbf4536fdc96
MeTeoR541 <105069731+MeTeoR541@users.noreply.github.com>
2022-05-08 13:21:58 +0800

    Add files via upload

--- Viewer.cpp
+++ Viewer.cpp
@@ -2,6 +2,24 @@
 Viewer::Viewer() {
 	return;
 }
-void Viewer::draw(const Board& board) {
-
+void Viewer::draw() {
+    QWidget* window = new QWidget();
+    QPushButton* choose1 = new QPushButton(QObject::tr("START"), window);
+    QPushButton* choose2 = new QPushButton(QObject::tr("LOAD"), window);
+    QPushButton* choose3 = new QPushButton(QObject::tr("END"), window);
+    choose1->setFixedHeight(50);
+    choose1->setFixedWidth(150);
+    choose2->setFixedHeight(50);
+    choose2->setFixedWidth(150);
+    choose3->setFixedHeight(50);
+    choose3->setFixedWidth(150);
+    QVBoxLayout* group = new QVBoxLayout;
+    group->addWidget(choose1);
+    group->addWidget(choose2);
+    group->addWidget(choose3);
+    group->setContentsMargins(50, 350, 200, 25);
+    window->setLayout(group);
+    window->setMinimumHeight(600);
+    window->setMinimumWidth(750);
+    window->show();
 }
--- Viewer.h
+++ Viewer.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <iostream>
+#include <QtWidgets>
+#include <QApplication>
+#include <QPushButton>
 #include "Board.h"
 using namespace std;
 class Viewer {
 public:
 	Viewer();
-	void draw(const Board& board);
+	void draw();
 };
